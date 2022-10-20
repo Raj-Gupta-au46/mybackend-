0171095625a70a618a87af43766da75d542809db
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2022-10-20 23:24:30 +0530

    param assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -160,4 +160,57 @@ router.post( "/post-query-2", function (req, res){
 })
 
 
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+//// assignment question  
+
+
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ 
+
+
+
+router.post('/voting', function(req,res){
+    let isAge = req.query.votingage
+    let eligible =persons.filter(x=>x.age>isAge)
+    
+    
+    for(let i=0;i<eligible.length;i++){
+
+        eligible[i].votingStatus=true;
+        
+    }
+        console.log(eligible);
+     res.send({data:eligible})
+})
+
+
+
 module.exports = router;
