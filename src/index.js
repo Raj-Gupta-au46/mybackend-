653537f5c6afcd818536ec231469172108fe69d4
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2022-10-27 17:08:08 +0530

    my incomplete assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rajgupta07082000:0Um5TBcHGam3DxeZ@cluster0.p92r9bx.mongodb.net/rajgupta07082000-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -4,13 +4,22 @@ const bookSchema = new mongoose.Schema( {
     bookName: String, 
     authorName: String, 
     tags: [String],
+    year:{
+        type:Number,
+        default:2021,
+    },
     
     isPublished: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    totalpages:{
+        type:Number,
+        required:true,
+    },
+    stockavailable:Boolean,
+    
 }, { timestamps: true });
 
 
